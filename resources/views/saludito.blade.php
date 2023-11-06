e5be938edcc6abe0feebc8f6ee7ca77b5af80ddb
Julio14sk <jvaldovino_20@alu.uabcs.mx>
2023-11-06 02:09:18 -0700

    Saludo, saludo desde view, suma, resta, producto, division

---
+++ resources/views/saludito.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        Hola guapo {{$name}}
+    </h1>
+
+    <p>
+        Tu sonrisa tan resplandeciente
+        A mi corazón deja encantado
+        Ven toma mi mano
+        Para huir de esta terrible oscuridad
+        En el instante en que te volví a encontrar
+        Mi mente trajo a mí aquel hermoso lugar
+        Que cuando era niño fue tan valioso para mí
+        Quiero saber si acaso tú conmigo quieres bailar
+        Si me das tu mano te llevaré
+        Por un camino cubierto de luz y oscuridad
+    </p>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/saludito/{name?}', function ($name="Hermoso") {
+    return view('saludito', ['name' => $name]);
+});
+
+Route::get('/saludo/{nombre}/{apellido?}', function (string $nombre,$apellido=" Mongolito") {
+    return 'hola '.$nombre.' '.$apellido;
+})->where(['nombre ' => '[a-z]+',' apellido' => '[a-z]+']);
+
+Route::get('/suma/{n1}/{n2}', function ($n1, $n2) {
+    return $n1 + $n2;
+})->where(['n1' => '[0-9]+',' n2' => '[0-9]+']);
+
+Route::get('/resta/{n1}/{n2}', function ($n1, $n2) {
+    return $n1 - $n2;
+})->where(['n1' => '[0-9]+',' n2' => '[0-9]+']);
+
+Route::get('/producto/{n1}/{n2}', function ($n1, $n2) {
+    return $n1 * $n2;
+})->where(['n1' => '[0-9]+',' n2' => '[0-9]+']);
+
+Route::get('/division/{n1}/{n2}', function ($n1, $n2) {
+    return $n1 / $n2;
+})->where(['n1' => '[0-9]+',' n2' => '[0-9]+']);
+
